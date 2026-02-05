f998a0dd95cc23ce60c2dbcec3b0e1fdbeac9936
kamilSharipov <kamil5812@gmail.com>
2026-02-05 19:41:31 +0300

    structure: add lib/cprefix, lib/fft, include dirs; issue #1

---
+++ include/prach.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <fftw3.h>
+
+namespace prach {
+
+const double PI = 3.14159265358979323846;
+const double E = 2.718281828459045;
+
+const size_t STANDART_NZC = 839;
+
+using Complex = fftw_complex;
+
+} // namespace prach
