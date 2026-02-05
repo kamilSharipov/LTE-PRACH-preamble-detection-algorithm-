c05152d9daabfa0a168e05b0f3f01e98c257b9c0
kamilSharipov <kamil5812@gmail.com>
2026-02-05 20:08:07 +0300

    feature: impemented cprefix insertation issue #9

--- lib/cprefix/cprefix.hpp
+++ lib/cprefix/cprefix.hpp
@@ -1,1 +1,13 @@
 #pragma once
+
+#include <vector>
+#include <cstdint>
+
+#include "prach.hpp"
+
+namespace prach {
+
+std::vector<Complex> add_cyclic_prefix(const std::vector<Complex>& signal,
+                                       size_t cp_length);
+
+} // namespace prach
