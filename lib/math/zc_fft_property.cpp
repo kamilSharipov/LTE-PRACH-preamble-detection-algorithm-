a39d7598924952964a3d59146fc6be843e081e35
kamilSharipov <kamil5812@gmail.com>
2026-02-06 19:01:20 +0300

    feature: implemented DFT for ZC sequence using property of ZC issue #18

---
+++ lib/math/zc_fft_property.cpp
@@ -0,0 +1,51 @@
+#include "zc_fft.property.hpp"
+
+#include "zc_generator.hpp"
+
+#include <cmath>
+
+namespace prach {
+
+int modular_inverse(int a, int mod) {
+    int t = 0, newt = 1;
+    int r = mod, newr = a;
+
+    while (newr != 0) {
+        int q = r / newr;
+
+        int tmp = newt;
+        newt = t - q * newt;
+        t = tmp;
+
+        tmp = newr;
+        newr = r - q * newr;
+        r = tmp;
+    }
+
+    if (r > 1) {
+        throw std::invalid_argument("lib/math/zc_fft_property.cpp: a is not invertible");
+    }
+
+    if (t < 0) {
+        t += mod;
+    }
+
+    return t;
+}
+
+std::vector<Complex> dft_via_zc_property(size_t Nzc, int u) {
+    if (u <= 0 || u >= Nzc) {
+        throw std::invalid_argument("u must be in [1, Nzc-1]");
+    }
+
+    const double phase = -PI * (Nzc - 1) / 2.0;
+    const Complex weight = Complex(cos(phase), sin(phase));
+
+    int new_u = ((-modular_inverse(u, Nzc)) % Nzc + Nzc) % Nzc;
+
+    std::vector<Complex> result = generate_zc_sequence(new_u, Nzc);
+
+    return result;
+}
+
+} // namespace prach
