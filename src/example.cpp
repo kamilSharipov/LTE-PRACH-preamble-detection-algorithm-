743c7717c722f3c9c46351bc0562504283dfea38
kamilSharipov <kamil5812@gmail.com>
2026-02-06 23:53:34 +0300

    fix: werror fixes

--- src/example.cpp
+++ src/example.cpp
@@ -1,6 +1,7 @@
 #include "fftw3.h"
 
 #include <iostream>
+#include <chrono>
 
 #include "zc_generator.hpp"
 #include "cprefix.hpp"
@@ -9,11 +10,18 @@
 using namespace prach;
 
 int main() {
-    std::vector<Complex> res = dft_via_zc_property(11, 3);
+    auto start = std::chrono::high_resolution_clock::now();
+    std::vector<Complex> res = dft_via_zc_property(839, 3);
+    auto end = std::chrono::high_resolution_clock::now();
 
-    for (auto el: res) {
-        std::cout << el << "\n";
-    }
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+    double avg_time_ms = static_cast<double>(duration.count()) / 1000.0;
+
+    std::cout << avg_time_ms << "\n";
+
+    //for (auto el: res) {
+        //std::cout << el << "\n";
+    //}
 
     return 0;
 }
