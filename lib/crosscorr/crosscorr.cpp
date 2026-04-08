7de722e56eec39b2eb3f5cf2a096285447378e3f
Kamil Sharipov <kamil5812@gmail.com>
2026-04-08 11:53:10 +0300

    refactoring: zero-padding logic moved to include/utils.hpp

--- lib/crosscorr/crosscorr.cpp
+++ lib/crosscorr/crosscorr.cpp
@@ -10,26 +10,10 @@ std::vector<Complex> count_cross_correlation(const std::vector<Complex>& x,
                                              const std::vector<Complex>& y,
                                              const size_t N) 
 {
-    size_t L               = y.size();
-    size_t left_part_size  = (L + 1) / 2;
-    size_t right_part_size = L / 2;
-
-    std::vector<Complex> Y_padded(N, Complex(0, 0));
-
-    std::copy(y.begin(),
-              y.begin() + left_part_size,
-              Y_padded.begin());
-
-    size_t right_start = N - right_part_size;
-
-    std::copy(y.begin() + left_part_size,
-              y.end(),
-              Y_padded.begin() + right_start);
-
     std::vector<Complex> res(N);
 
     for (size_t i = 0; i < N; i++) {
-        res[i] = x[i] * std::conj(Y_padded[i]);
+        res[i] = x[i] * std::conj(y[i]);
     }
 
     std::vector<Complex> ifft_res = ifft_fftw(res, N);
